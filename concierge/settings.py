663b6fe2abccab6a84a380d1423d89a72153864e
Nickolaus D. Saint <nick@globus.org>
2020-09-09 15:56:39 -0700

    Removed unused import from settings

--- concierge/settings.py
+++ concierge/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 import os
-import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
